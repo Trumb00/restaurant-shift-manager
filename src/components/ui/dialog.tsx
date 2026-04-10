c743c5142bcd1bd8d27737c3bca5206ed0df635d
Trumb00 <umberto.clll@gmail.com>
2026-04-10 17:36:31 +0200

    fix: suppress Radix Dialog aria-describedby warning globally

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -34,6 +34,7 @@ const DialogContent = React.forwardRef<
   <DialogPortal>
     <DialogOverlay />
     <DialogPrimitive.Content
+      aria-describedby={undefined}
       ref={ref}
       className={cn(
         'fixed left-1/2 top-1/2 z-50 w-full max-w-lg -translate-x-1/2 -translate-y-1/2',
